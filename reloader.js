e7a823e67a57865039dfe989a536922882052cdc
mak <makindosx@gmail.com>
2019-05-23 04:23:55 +0300

    Add files via upload

--- reloader.js
+++ reloader.js
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2016-2019 Barchampas Gerasimos <http://chat.openloadlinks.com>
+ * anon-chat-v2 is a program that allows anonymous conversations.
+ *
+ * anon-chat-v2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ *
+ * anon-chat-v2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+
 var req;
 
 function reloadData()
